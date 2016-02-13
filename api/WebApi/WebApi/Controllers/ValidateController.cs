0227719405d135a2601943a1c8d5fc12f487fdfb
Jeff Linton <jslinton44@gmail.com>
2016-02-13 01:10:36 -0700

    Renames web api endpoints

--- api/WebApi/WebApi/Controllers/ValidateController.cs
+++ api/WebApi/WebApi/Controllers/ValidateController.cs
@@ -1,27 +1,27 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 using System.Text.RegularExpressions;
 using System.Linq;
 using System.Threading;
 
 using WebApi.Models;
-using System;
 
 namespace WebApi.Controllers
 {
-    [RoutePrefix("test")]
-    public class TestController : ApiController
+    [RoutePrefix("validate")]
+    public class ValidateController : ApiController
     {
         private readonly Random _random = new Random();
 
-        private const int delay = 500;
-        private const string emailPattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        private const int _delay = 500;
+        private const string _emailPattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
 
         [Route("email"), HttpGet]
         public IHttpActionResult Index(string email)
         {
-            Thread.Sleep(_random.Next(delay) + delay/2);
+            Thread.Sleep(_random.Next(_delay) + _delay/2);
 
-            var isValidEmail = Regex.Match(email, emailPattern).Success;
+            var isValidEmail = Regex.Match(email, _emailPattern).Success;
 
             var result = new ValidationResult
             {
@@ -37,7 +37,7 @@ namespace WebApi.Controllers
         [Route("username"), HttpGet]
         public IHttpActionResult Username(string username)
         {
-            Thread.Sleep(_random.Next(delay) + delay / 2);
+            Thread.Sleep(_random.Next(_delay) + _delay / 2);
 
             var userNameAvailable = !userNameList.Contains(username);
 
