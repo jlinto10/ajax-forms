2684250b9d17fd0d9825b9e0cadc17b897c0c5e1
Jeff Linton <jslinton44@gmail.com>
2016-04-04 07:29:59 -0600

    Removes the validate email endpoint and example

--- api/WebApi/WebApi/Controllers/ValidateController.cs
+++ api/WebApi/WebApi/Controllers/ValidateController.cs
@@ -14,23 +14,6 @@ namespace WebApi.Controllers
         private readonly Random _random = new Random();
 
         private const int _delay = 500;
-        private const string _emailPattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
-
-        [Route("email"), HttpGet]
-        public IHttpActionResult Index(string email)
-        {
-            Thread.Sleep(_random.Next(_delay) + _delay/2);
-
-            var isValidEmail = Regex.Match(email, _emailPattern).Success;
-
-            var result = new ValidationResult
-            {
-                isValid = isValidEmail,
-                message = isValidEmail ? "" : string.Format("{0} is not a valid email address", email)
-            };
-
-            return Ok(result);
-        }
 
         private string[] userNameList = new []{ "test", "username" };
 
